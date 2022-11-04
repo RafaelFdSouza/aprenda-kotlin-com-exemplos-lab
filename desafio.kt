4e0def53c17c46aae308127654b0af7a601b92da
Rafael Ferreira de Souza <90228022+RafaelFdSouza@users.noreply.github.com>
2022-11-04 19:19:22 -0300

    Entrega desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val email: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario) {       
+        inscritos.add(usuario)
+    }
+    
+    fun inscritosPorNome(){
+        for(inscrito in inscritos){
+            println(inscrito.nome)
+        }
+    }
+     fun conteudosFormacao(){
+        for(conteudo in conteudos){
+            println("${conteudo.nome} : ${conteudo.duracao} horas")
+        }
     }
 }
 
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+       
+    //Criar usuarios
+    val rafael = Usuario("Rafael", "rafael.gmail.com")
+    val maria  = Usuario("Maria", "maria.outlook.com")
+    val rita = Usuario("Rita", "rita.outlook.com")
+    
+    //Criar conteudos
+    val oopKotlin = ConteudoEducacional("Orientação a Objetos com Kotlin", 8)
+    val jetpack = ConteudoEducacional("Dominando o Android Jetpack", 10)
+    
+    val listaConteudosAndroid = mutableListOf<ConteudoEducacional>()
+    listaConteudosAndroid.add(oopKotlin)
+    listaConteudosAndroid.add(jetpack)
+    
+    //Criar formacao
+    val formacaoAndroid = Formacao("Formação Android Developer", listaConteudosAndroid, Nivel.AVANCADO)
+    
+    //Simulando matricula
+    formacaoAndroid.matricular(rafael)
+    formacaoAndroid.matricular(rita)  
+    
+    println("Os seguintes usuários estão matriculados em ${formacaoAndroid.nome} :")
+    formacaoAndroid.inscritosPorNome()
+    println("Conteúdos da formação: ")
+    
+    println("${formacaoAndroid.conteudosFormacao()}")
+    println("${formacaoAndroid.nivel}")
+ }
+    
